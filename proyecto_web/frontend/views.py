06fb90f9c6efdafc9cacb40bff52c1ead6b910b1
Der Basilisk <93922687+TheDeadWalker@users.noreply.github.com>
2026-02-23 09:25:51 -0500

    tailwind

--- proyecto_web/frontend/views.py
+++ proyecto_web/frontend/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+def home(request):
+    return render(request, 'frontend/home.html')
 # Create your views here.
